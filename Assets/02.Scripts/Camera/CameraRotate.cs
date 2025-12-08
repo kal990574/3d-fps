69938d0f421b05940a28a96f088004a03cf54292
mambasa <101706131+kal990574@users.noreply.github.com>
2025-12-08 15:01:28 +0900

    feat : camera 기본 rotate 설정

---
+++ Assets/02.Scripts/Camera/CameraRotate.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float RotationSpeed = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+        Vector3 direction = new Vector3(-mouseY, mouseX, 0).normalized;
+        transform.eulerAngles += RotationSpeed * direction * Time.deltaTime;
+    }
+}
