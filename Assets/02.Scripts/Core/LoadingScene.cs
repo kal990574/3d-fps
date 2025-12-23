f27c1db925ecbc4dffb0147b25f0a8d4b868f237
mambasa <101706131+kal990574@users.noreply.github.com>
2025-12-23 16:02:17 +0900

    feat: loading scene 구현

---
+++ Assets/02.Scripts/Core/LoadingScene.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadingScene : MonoBehaviour
+{
+    [SerializeField] private Slider _progressSlider;
+    [SerializeField] private TextMeshProUGUI _progressText;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene_Coroutine());
+    }
+
+    private IEnumerator LoadScene_Coroutine()
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync("GameScene");
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            _progressSlider.value = ao.progress;
+            _progressText.text = ao.progress + "%";
+            if (ao.progress >= 0.9f)
+            {
+                ao.allowSceneActivation = true;
+            }
+            yield return null;
+        }
+    }
+}
